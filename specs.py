2f7597816d6ad56384f7e3ddb834368d2da2068e
Kevin <izzamkvinn@gmail.com>
2026-08-05 21:52:29 +0700

    fix typo

---
+++ specs.py
@@ -0,0 +1,8 @@
+import platform
+
+print("system:", platform.system())
+print("node name:", platform.node())
+print("release:", platform.release())
+print("version:", platform.version())
+print("machine:", platform.machine())
+print("processor:", platform.processor())
